0e0ade7f24702e8667c58822eacc525ce42787da
Đặng Cao Trí <no12spr@gmail.com>
2024-06-30 18:20:42 +0700

    Add logout

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -64,6 +64,20 @@ const loginUser = async (req, res) => {
     }
 }
 
+const logoutUser = async (req, res) => {
+    try {
+        res.clearCookie('refresh_token')
+        return res.status(200).json({
+            status: 'OK',
+            message: 'You are log out'
+        })
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
 const updateUser = async (req, res) => {
     try {
         const userId = req.params.id
@@ -131,7 +145,6 @@ const getDetailsUser = async (req, res) => {
 }
 
 const refreshToken = async (req, res) => {
-    console.log('req.cookies.refresh_token', req.cookies.refresh_token)
     try {
         const token = req.cookies.refresh_token
         if (!token) {
@@ -157,5 +170,6 @@ module.exports = {
     deleteUser,
     getAllUser,
     getDetailsUser,
-    refreshToken
+    refreshToken,
+    logoutUser
 }
--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -5,6 +5,7 @@ const { authMiddleware, authUserMiddleware } = require("../middleware/authMiddle
 
 router.post('/sign-up', UserController.createUser);
 router.post('/sign-in', UserController.loginUser);
+router.post('/log-out', UserController.logoutUser);
 router.put('/update-user/:id', UserController.updateUser);
 router.delete('/delete-user/:id', authMiddleware, UserController.deleteUser);
 router.get('/get-all', authMiddleware ,UserController.getAllUser);
